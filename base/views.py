e2ebfefbb0c63057eab58f22fefba868166e33b3
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-20 16:32:04 +0100

    Django

--- base/views.py
+++ base/views.py
@@ -6,13 +6,13 @@ rooms = [
     {'id': 1, 'name': "Let us learn python"},
     {'id': 1, 'name': "Design with me"},
     {'id': 1, 'name': "Frontend developer"},
-]
+] 
 
 def home(request):
     context = {'rooms': rooms}
-    return render(request, 'index.html', context)
+    return render(request, 'base/index.html', context)
 
 def room(request):
-    return render(request,'room.html')
+    return render(request,'base/room.html')
 
 # Create your views here.
