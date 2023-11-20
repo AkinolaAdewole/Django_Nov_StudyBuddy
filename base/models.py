1f16dd80d19e8430cc6777a4bdb3b1aa59eeba16
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-20 19:05:33 +0100

    Django

--- base/models.py
+++ base/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Room(models.Model):
+    # host =
+    # topic =
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
+    # participant =
+    updated = models.DateTimeField(auto_now=True)
