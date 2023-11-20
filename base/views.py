0814d4d5aef51f8e0b5517fef32c8b5b6b399b7b
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-20 16:07:48 +0100

    Django

--- base/views.py
+++ base/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+rooms = [
+    {'id': 1, 'name': "Let us learn python"},
+    {'id': 1, 'name': "Design with me"},
+    {'id': 1, 'name': "Frontend developer"},
+]
+
 def home(request):
-    return render(request, 'index.html' )
+    return render(request, 'index.html', {'rooms': rooms})
 
 def room(request):
     return render(request,'room.html')
