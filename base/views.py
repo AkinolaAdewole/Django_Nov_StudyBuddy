95e5ddf8bba2786fe32b5ce532d079110d48c3d4
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-20 17:07:33 +0100

    Django

--- base/views.py
+++ base/views.py
@@ -4,15 +4,21 @@ from django.http import HttpResponse
 
 rooms = [
     {'id': 1, 'name': "Let us learn python"},
-    {'id': 1, 'name': "Design with me"},
-    {'id': 1, 'name': "Frontend developer"},
+    {'id': 2, 'name': "Design with me"},
+    {'id': 3, 'name': "Frontend developer"},
 ] 
 
 def home(request):
     context = {'rooms': rooms}
     return render(request, 'base/index.html', context)
 
-def room(request):
-    return render(request,'base/room.html')
+def room(request, pk):
+    room = None
+
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = 1
+    context = room
+    return render(request,'base/room.html', context)
 
 # Create your views here.
