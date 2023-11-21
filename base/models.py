fe231c423922823b88ca974fcd6f2b9251a0f399
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-21 13:50:08 +0100

    Django Programming

--- base/models.py
+++ base/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
 class Room(models.Model):
     # host =
     # topic =
@@ -17,7 +20,7 @@ class Room(models.Model):
     
 
 class Message(models.Model):
-    # user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
