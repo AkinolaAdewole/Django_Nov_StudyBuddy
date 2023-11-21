e3a1f17f54a92094352ca25f26f37d258defd70b
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-21 19:34:17 +0100

    Django Programming

--- base/views.py
+++ base/views.py
@@ -28,6 +28,11 @@ def room(request, pk):
 
 def createRoom(request):
     form = RoomForm()
+    if request.method == 'POST':
+        # print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
