a8a2849912061845c08918a47bd7d58f2d4b750b
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-23 14:19:03 +0100

    Backend (Django)

--- base/urls.py
+++ base/urls.py
@@ -7,5 +7,6 @@ urlpatterns =[
     path('room/<str:pk>/', views.room, name="room"),
     #   'room/<str:pk>/ -- dynamic routing
     path('create-room/', views.createRoom, name="create-room"),
-    path('update-room/<str:pk>/', views.updateRoom, name="update-room")
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room")
 ]
