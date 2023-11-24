0e5023d206d74ce65529e09adf616843f33e777e
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-24 21:25:20 +0100

    Backend (Django)

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
@@ -12,7 +12,8 @@ from .forms import RoomForm
 
 def home(request):
     rooms= Room.objects.all()
-    context = {'rooms': rooms}
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics':topics}
     return render(request, 'base/index.html', context)
 
 def room(request, pk):
